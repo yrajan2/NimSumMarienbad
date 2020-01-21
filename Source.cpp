74835fa40c0b06562c799862495ceac09fbdc8dd
Yaqzaan <59808048+yrajan2@users.noreply.github.com>
2020-01-21 10:03:52 +0800

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,290 +1,132 @@
 #include <iostream>
-#include <cstdlib>
 using namespace std;
 
-int row[4] = { 1, 3, 5, 7 };
-bool gameover;
-char turn;
+int row[2] = { 8, 24 };
 
-void Pturn() {
-	int crow, cstar;
-	cout << "Enter your chosen row." << endl;
-	cin >> crow;
-	while (crow < 1 || crow > 4 || row[crow-1] == 0)
+void printBoard(int a, int b) {
+	int grow = 0;
+	int gcol = 24;
+	for (int i = 0; i < 9; i++)
 	{
-		cout << "Please enter a valid row number between 1 to 4 which has stars left." << endl;
-		cin >> crow;
-	}
-	cout << "Enter the number of stars you want to remove." << endl;
-	cin >> cstar;
-	while (cstar < 1 || cstar > row[crow-1])
-	{
-		cout << "Please enter a valid star number considering the starts left in row " << crow << endl;
-		cin >> cstar;
-	}
-	row[crow-1] -= cstar;
-}
-
-void CheckStatus() {
-	if (row[0] == 0 && row[1] == 0 && row[2] == 0 && row[3] == 0)
-	{
-		gameover = true;
-		if (turn == 'Y')
+		for (int j = 0; j < 25; j++)
 		{
-			cout << "Sorry, You Lost";
-		}
-		else
-		{
-			cout << "Congrats, you won!!";
-		}
-	}
-	else
-	{
-		for (int i = 0; i < 4; i++)
-		{
-			for (int j = 0; j < row[i]; j++)
+			if (a == i && j == b)
+			{
+				cout << "R";
+			}
+			else if (grow == i && gcol == j)
+			{
+				cout << "G";
+			}
+			else
 			{
 				cout << "*";
 			}
-			cout << endl;
 		}
+		cout << endl;
 	}
 }
 
-int findBinary(int number) {
-	switch (number)
+bool checkStatus(int a, int b) {
+	if (a == 0 && b == 24)
 	{
-	case 0:
-		return 1000;
-	case 1:
-		return 1001;
-	case 2:
-		return 1010;
-	case 3:
-		return 1011;
-	case 4:
-		return 1100;
-	case 5:
-		return 1101;
-	case 6:
-		return 1110;
-	case 7:
-		return 1111;
-	}
-}
-
-void Myturn(){
-	/*Make Nim Sum Calculator now*/
-	int rowbin[4];
-	int nimsum, mrow, mstar, emptycount, lastcount, finmove;
-	emptycount = 0;
-	lastcount = 0;
-	finmove = 0;
-	for (int i = 0; i < 4; i++)
-	{
-		rowbin[i] = findBinary(row[i]);
-		//cout << rowbin[i] << endl;
-	}
-	nimsum = rowbin[0] ^ rowbin[1] ^ rowbin[2] ^ rowbin[3];
-	//cout << nimsum << endl;
-	cout << "Next Move" << endl;
-	for (int j = 0; j < 4; j++)
-	{
-		if (row[j] > 1)
-		{
-			lastcount += 1;
-			finmove = j;
-		}
-		else if (row[j] == 0)
-		{
-			emptycount += 1;
-		}
-	}
-	if (lastcount == 1)
-	{
-		if (emptycount == 3)
-		{
-			row[finmove] = 1;
-		}
-		else
-		{
-			row[finmove] = 0;
-		}
+		return false;
 	}
 	else
 	{
-		switch (nimsum)
+		return true;
+	}
+}
+
+void Pturn(int &a, int &b) {
+	cout << "Do you want to move the rook horizontally or vertically?" << endl << "1. vertically" << endl << "2. Horizontally" << endl;
+	int move;
+	do
+	{
+		cin >> move;
+	} while (move != 1 && move != 2);
+	int steps;
+	cout << "How many steps do you want to move?" << endl;
+	do
+	{
+		cin >> steps;
+	} while (steps < 1 || steps > row[move-1]);
+	row[move - 1] -= steps;
+	if (move == 1)
+	{
+		a -= steps;
+	}
+	else
+	{
+		b += steps;
+	}
+}
+
+void Mturn(int &a, int &b) {
+	int steps;
+	if (row[0] > row[1])
+	{
+		steps = row[0] - row[1];
+		a -= steps;
+		row[0] = row[1];
+	}
+	else if (row[1] > row[0])
+	{
+		steps = row[1] - row[0];
+		b += steps;
+		row[1] = row[0];
+	}
+	else
+	{
+		steps = 1;
+		if (row[1] != 0)
 		{
-		case 0:
-			mrow = rand() % 4;
-			while (row[mrow] == 0)
-			{
-				mrow = rand() % 4;
-			}
-			mstar = rand() % row[mrow] + 1;
-			row[mrow] -= mstar;
-			break;
-		case 1:
-			for (int i = 0; i < 4; i++)
-			{
-				if (rowbin[i] % 2 != 0)
-				{
-					mrow = i;
-					break;
-				}
-			}
-			mstar = 1;
-			row[mrow] -= mstar;
-			break;
-		case 26:
-			mrow = rand() % 4;
-			while (row[mrow] <= 1)
-			{
-				mrow = rand() % 4;
-			}
-			mstar = 2;
-			row[mrow] -= mstar;
-			break;
-		case 27:
-			mrow = 3;
-			if (row[mrow] == 7)
-			{
-				mstar = 3;
-			}
-			else if (row[mrow] == 3)
-			{
-				mstar = 2;
-			}
-			else
-			{
-				for (int k = 1; k < 3; k++)
-				{
-					if (row[k] == 3) {
-						mrow = k;
-						mstar = 3;
-						break;
-					}
-				}
-			}
-			if (mstar != 3 && mstar != 2)
-			{
-				mrow = rand() % 4;
-				while (row[mrow] != 2)
-				{
-					mrow = rand() % 4;
-				}
-				mstar = 1;
-			}
-			row[mrow] -= mstar;
-			break;
-		case 1956:
-			mrow = rand() % 4;
-			while (row[mrow] <= 3)
-			{
-				mrow = rand() % 4;
-			}
-			mstar = 4;
-			row[mrow] -= mstar;
-			break;
-		case 1957:
-			if (row[3] >= 4)
-			{
-				mrow = 3;
-			}
-			else
-			{
-				mrow = 2;
-			}
-			if (row[mrow] % 2 == 0)
-			{
-				mstar = 3;
-			}
-			else
-			{
-				mstar = 5;
-			}
-			row[mrow] -= mstar;
-			break;
-		case 1982:
-			if (row[3] >= 4)
-			{
-				mrow = 3;
-				if (row[mrow] <= 5)
-				{
-					mstar = 3;
-				}
-				else
-				{
-					mstar = 6;
-				}
-			}
-			else
-			{
-				mrow = 2;
-				mstar = 2;
-			}
-			row[mrow] -= mstar;
-			break;
-		case 1983:
-			if (row[3] >= 4)
-			{
-				mrow = 3;
-			}
-			else
-			{
-				mrow = 2;
-			}
-			if (row[mrow] == 4)
-			{
-				mstar = 2;
-			}
-			else if (row[mrow] == 7)
-			{
-				mstar = 7;
-			}
-			else if (row[mrow] == 6)
-			{
-				mstar = 5;
-			}
-			{
-				mstar = 3;
-			}
-			row[mrow] -= mstar;
-			break;
+			row[1] -= steps;
+			b += steps;
+		}
+		else
+		{
+			row[0] -= steps;
+			a -= steps;
 		}
 	}
 }
 
 int main() {
-	for (int i = 0; i < 4; i++)
+	cout << "Welcome to the rooks game!!" << endl;
+	cout << "In this game, you have a 9 * 25 chess board. Your goal is to move the rook from the bottom left to the top right corner. The rook can move in the same manner as it does in chess." << endl;
+	cout << "The illustration is shown below. R is the Rook's position" << endl;
+	int rrow = 8;
+	int rcol = 0;
+	int turn = 0;
+	printBoard(rrow, rcol);
+	cout << "Would you like to take the first turn?" << endl << "1. Yes" << endl << "2. No" << endl;
+	while (turn != 1 && turn != 2)
 	{
-		for (int j = 0; j < row[i]; j++)
-		{
-			cout << "*";
-		}
-		cout << endl;
-	}
-	cout << "Rules: In each turn, you can pick as many stars as you want from one single row. The player who picks the last star loses. Would you like to go first? (Y or N)" << endl;
-	cin >> turn;
-	while (turn != 'Y' && turn != 'N')
-	{
-		cout << "Enter Y or N only!" << endl;
 		cin >> turn;
 	}
-	gameover = false;
-	while (!gameover)
+	while (checkStatus(rrow, rcol))
 	{
-		if (turn == 'Y')
+		if (turn == 1)
 		{
-			Pturn();
-			CheckStatus();
-			turn = 'N';
+			Pturn(rrow, rcol);
+			printBoard(rrow, rcol);
+			turn = 2;
 		}
 		else
 		{
-			Myturn();
-			CheckStatus();
-			turn = 'Y';
+			Mturn(rrow, rcol);
+			cout << "My turn" << endl;
+			printBoard(rrow, rcol);
+			turn = 1;
 		}
+	}
+	if (turn == 1)
+	{
+		cout << "Sorry you lost!";
+	}
+	else
+	{
+		cout << "Congrats! You Won!";
 	}
 }
